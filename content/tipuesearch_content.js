21e4dc931e878cd92388633d2d003b59ec5de26d
nfu40923241 <40923241@nfu.edu.tw>
2023-03-16 15:41:30 +0800

    may i umm

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n test \n ======= \n text \n 我倒是想看看能不能再來一次！ \n ======= \n 終於，SSH要成功了嗎? 20230316 By 40723217', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n test \n ======= \n text \n 我倒是想看看能不能再來一次！ \n ======= \n 終於，SSH要成功了嗎? 20230316 By 40723217 \n ======= \n 這次嘗試使用網頁acp 0308 \n 讓我推啦啦啦啦吼', 'tags': '', 'url': 'About.html'}]};
